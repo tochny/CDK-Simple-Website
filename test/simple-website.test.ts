ecc34c7186d2c2ce80c8dac49633aaeeda399ec5
Alex Chih <alex.ch.chih@viewsonic.com>
2024-05-03 16:47:44 +0800

    Initial commit

---
+++ test/simple-website.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SimpleWebsite from '../lib/simple-website-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/simple-website-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SimpleWebsite.SimpleWebsiteStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
